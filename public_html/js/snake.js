79b6cf85940ecdd78dd459e3f51ff84ae0e906d5
Ivan Salgado <ivansalgado2000@gmail.com>
2014-10-07 09:18:15 -0700

    added new functions

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -3,3 +3,15 @@ var snake;
 var context;
 var screenWidth;
 var screenHeight;
+
+function gameInitialize () {
+    var canvas = document.getElementById ("game-screen");
+}
+
+function gameLoop () {
+    
+}
+
+function gameDraw () {
+    
+}

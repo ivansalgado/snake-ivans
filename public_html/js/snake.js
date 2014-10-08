6996dd4f898f63260e44ce54127631474c06406d
Ivan Salgado <ivansalgado2000@gmail.com>
2014-10-08 09:19:33 -0700

    using gameInitialize and gameDraw

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -6,6 +6,13 @@ var screenHeight;
 
 function gameInitialize () {
     var canvas = document.getElementById ("game-screen");
+    context = cavas.getContext ("2d");
+    
+    screenWidth = window.innerWidth;
+    screenHeight = window.innerHeight;
+    
+    canvas.width = screenWidth;
+    canvas.height = screenHeight;
 }
 
 function gameLoop () {
@@ -13,5 +20,6 @@ function gameLoop () {
 }
 
 function gameDraw () {
-    
+    context.fillStyle = "rgb(179, 234, 245)";
+    context.fillSqr = ;
 }

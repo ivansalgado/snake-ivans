b53e16ba1b1f5c218d059cd768c7e39a000156a8
Ivan Salgado <ivansalgado2000@gmail.com>
2014-10-10 09:19:25 -0700

    added snakeInitialize and snake Draw

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -4,6 +4,9 @@ var context;
 var screenWidth;
 var screenHeight;
 
+var snakeLength;
+var snakeSize;
+
 gameInitialize ();
 gameDraw ();
 
@@ -26,3 +29,16 @@ function gameDraw () {
     context.fillStyle = "rgb(139, 195, 74)";
     context.fillRect(0, 0, screenWidth, screenHeight);
 }
+
+
+function snakeInitialize () {
+    snake = [];
+    snakeLength = 15;
+    snakeSize = 20;
+    
+    for(var index = 0; index < snakeLength; index++){} 
+}
+
+function snakeDraw () {}
+
+function snakeUpdate () {}

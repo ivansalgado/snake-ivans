094eebe8ed45abb5be2ff3db64d433945971b811
Ivan Salgado <ivansalgado2000@gmail.com>
2014-10-16 09:16:19 -0700

    finally added the snake

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -8,7 +8,9 @@ var snakeLength;
 var snakeSize;
 
 gameInitialize ();
+snakeInitialize ();
 gameDraw ();
+snakeDraw ();
 
 function gameInitialize () {
     var canvas = document.getElementById ("game-screen");
@@ -37,10 +39,16 @@ function snakeInitialize () {
     snakeSize = 20;
     
     for(var index = 0; index < snakeLength; index++){
-        snake.push ()
+        snake.push ({
+            x: index,
+            y: 0
+        });
     } 
 }
 
-function snakeDraw () {}
+function snakeDraw () { for(var index = 0; index < snakeLength; index++) 
+        context.fillStyle = "white";
+        context.fillRect(snake[index].x * snakeSnake, snake[index].y * snakeSize,snakeSize, snakeSize);
+}
 
 function snakeUpdate () {}

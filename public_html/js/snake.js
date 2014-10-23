84bb806081e3099fc84e451e047a7f96ee27fc01
Ivan Salgado <ivansalgado2000@gmail.com>
2014-10-23 09:18:09 -0700

    changed the direction of the snake

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -1,8 +1,14 @@
+/* ___________________________________________________________________________
+ * Variables                                                                 /
+ * __________________________________________________________________________\
+ */
+
 var snake;
     
 var context;
 var screenWidth;
 var screenHeight;
+var snakeDirection;
 
 var food;
 
@@ -11,10 +17,21 @@ var snakeSize;
 var snakeHead;
 var snakeTail;
 
+/* ___________________________________________________________________________
+ * Excecuting game code                                                      /
+ * __________________________________________________________________________\
+ */
+
 gameInitialize ();
 snakeInitialize (); 
 foodInitialize ();
 setInterval (gameLoop, 1000/25);
+setFoodPosition ();
+
+/* ___________________________________________________________________________
+ * Dimensions of the game screen                                             /
+ * __________________________________________________________________________\
+ */
 
 function gameInitialize () {
     var canvas = document.getElementById ("game-screen");
@@ -27,6 +44,11 @@ function gameInitialize () {
     canvas.height = screenHeight;
 }
 
+/* ___________________________________________________________________________
+ * Updates the snake / Moves food                                            /
+ * __________________________________________________________________________\
+ */ 
+
 function gameLoop () {
     gameDraw ();
     snakeUpdate ();
@@ -34,16 +56,26 @@ function gameLoop () {
     foodDraw ();
 }
 
+/* ___________________________________________________________________________
+ * Gives the style of the game                                               /
+ * __________________________________________________________________________\
+ */
+
 function gameDraw () {
     context.fillStyle = "rgb(128, 222, 234)";
     context.fillRect(0, 0, screenWidth, screenHeight);
 }
 
+/* ___________________________________________________________________________
+ * Gives the initial length of the snake                                     /       
+ * __________________________________________________________________________\
+ */                                                                                                                                                                                                                  
 
 function snakeInitialize () {
     snake = [];
     snakeLength = 5;
     snakeSize = 20;
+    snakeDirection = "down";
     
     for(var index = snakeLength - 1; index >= 0; index--){
         snake.push ({
@@ -53,6 +85,11 @@ function snakeInitialize () {
     } 
 }
 
+/* ___________________________________________________________________________
+ * Gives the style of the snake                                              /
+ * __________________________________________________________________________\
+ */
+
 function snakeDraw () {
     for(var index = 0; index < snake.length; index++) {
         context.fillStyle = "rgb(0, 0, 38)";
@@ -60,11 +97,16 @@ function snakeDraw () {
     }
 }
 
+/* ___________________________________________________________________________
+ * Updates the length of the snake                                           /
+ * __________________________________________________________________________\
+ */
+
 function snakeUpdate () {
     var snakeHeadX = snake[0].x;
     var snakeHeadY = snake[0].y;
     
-    snakeHeadX++;
+    snakeHeadY++;
     
     var snakeTail = snake.pop();
     snakeTail.x = snakeHeadX;
@@ -72,14 +114,38 @@ function snakeUpdate () {
     snake.unshift(snakeTail);
 }
 
+/* ___________________________________________________________________________
+ * Gives the location food on the screen                                     /
+ * __________________________________________________________________________\
+ */
+
 function foodInitialize(){
     food = {
       x: 0,
       y: 0
     };
+    setFoodPosition;
 }
+
+/* ___________________________________________________________________________
+ * Gives the style of the food                                               /
+ * __________________________________________________________________________\
+ */
 
 function foodDraw() {
     context.fillStyle = "rgb(0, 0, 38)";
     context.fillRect(food.x, food.y, snakeSize, snakeSize);
+}
+
+/* ___________________________________________________________________________
+ * Randomly positions the food                                               /
+ * __________________________________________________________________________\
+ */
+
+function setFoodPosition () {
+    var randomX = Math.floor(Math.random() * screenWidth);
+    var randomY = Math.floor(Math.random() * screenHeight);
+    
+    food.x = randomX;
+    food.y = randomY;
 }
